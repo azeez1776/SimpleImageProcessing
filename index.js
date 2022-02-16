ca3d4f0823a70b346f7182ed6a25a95220d8fae7
azeez1776 <aboud3am81@gmail.com>
2022-02-16 13:38:27 +0300

    made a function to add brightness

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const caman = require('caman').Caman;
+
+caman('./assets/input/lofi.jpg', function(){
+    this.brightness(5);
+    this.render(function(){
+        this.save('./assets/output/lofi.jpg');
+    })
+})
