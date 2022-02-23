18eaa8874a6af613ddf2e9d871300dc43cc3a246
azeez1776 <aboud3am81@gmail.com>
2022-02-23 23:09:24 +0300

    fixed testing issue

--- __test__/funcs.spec.js
+++ __test__/funcs.spec.js
@@ -14,47 +14,47 @@ const { gamma,
 describe("testing the simp functions", () => {
 
     test("testing for gamma function", ()=>{
-        expect(gamma('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(gamma('../assets/input/aloha.png', '../assets/output/gamma.png',6)).toBeDefined()
     });
 
     test("testing for brightness function", ()=>{
-        expect(brightness('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(brightness('../assets/input/aloha.png', '../assets/output/brightness.png',6)).toBeDefined()
     });
 
     test("testing for greyscale function", () => {
-        expect(greyscale('../assets/input/aloha.jpg', '../assets/output/gamma.jpg')).toBeDefined()
+        expect(greyscale('../assets/input/aloha.png', '../assets/output/greyscale.png')).toBeDefined()
     });
 
     test("testing for contrast function", () => {
-        expect(contrast('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(contrast('../assets/input/aloha.png', '../assets/output/contrast.png',6)).toBeDefined()
     });
 
     test("testing for sepia function", ()=> {
-        expect(sepia('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(sepia('../assets/input/aloha.png', '../assets/output/sepia.png',6)).toBeDefined()
     });
 
     test("testing for vibrance function", () => {
-        expect(vibrance('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(vibrance('../assets/input/aloha.png', '../assets/output/vibrance.png',6)).toBeDefined()
     });
 
     test("testing for saturation function", () => {
-        expect(saturation('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(saturation('../assets/input/aloha.png', '../assets/output/saturation.png',6)).toBeDefined()
     });
 
     test("testing for hue function", () => {
-        expect(hue('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(hue('../assets/input/aloha.png', '../assets/output/hue.png',6)).toBeDefined()
     });
 
     test("testing the invert function", () => {
-        expect(invert('../assets/input/aloha.jpg', '../assets/output/gamma.jpg')).toBeDefined()
+        expect(invert('../assets/input/aloha.png', '../assets/output/invert.png')).toBeDefined()
     });
 
     test("testing the noise function", () => {
-        expect(noise('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(noise('../assets/input/aloha.png', '../assets/output/noise.png',6)).toBeDefined()
     });
 
     test("testing for the fillColor function", () => {
-        expect(fillColor('../assets/input/aloha.jpg', '../assets/output/gamma.jpg',6)).toBeDefined()
+        expect(fillColor('../assets/input/aloha.png', '../assets/output/fillColor.png',6)).toBeDefined()
     });
 
 
